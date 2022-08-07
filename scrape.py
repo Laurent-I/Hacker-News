ccb1cc1b393393990708a0f408299e7b2217e27f
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-07 02:20:06 +0200

    Scraping Hackers News For Stories with over 100 points in descending order on one page

---
+++ scrape.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+res = requests.get('https://news.ycombinator.com/news')
+# res2 = requests.get('https://news.ycombinator.com/news?p=2')
+soup = BeautifulSoup(res.text, 'html.parser')
+# soup2 = BeautifulSoup(res2.text, 'html.parser')
+links = soup.select('.titlelink')
+subtext = soup.select('.subtext')
+# links2 = soup2.select('.titlelink')
+# subtext2 = soup2.select('.subtext')
+
+# mega_links = links + links2
+# mega_subtext = subtext + subtext2
+
+
+def sort_stories(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for index, item in enumerate(links):
+
+        title = item.getText()
+        href = item.get('href', None)
+        votes = subtext[index].select('.score')
+        if len(votes):
+            points = int(votes[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories(hn)
+
+
+pprint.pprint(create_custom_hn(links, subtext))
